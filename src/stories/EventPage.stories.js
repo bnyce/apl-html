975b01955963905e2f045fd485fb856ffe3f2590
Bryce Benton <you@example.com>
2022-05-11 16:36:30 -0500

    css cleanups

--- src/stories/EventPage.stories.js
+++ src/stories/EventPage.stories.js
@@ -1,17 +1,17 @@
 //import { createEventPage } from './EventPage';
 import { getNodeString, getNavString, getTagString, getCalendarString } from './Event';
 import { getNavigationBarString, getHeroString } from './APLHeader';
+import { getFooterString } from './APLFooter';
 
 export default {
   title: 'Events/EventPage',
 };
 
-//const Template = (args) => createEventPage(args);
-
 const myCalendar = getCalendarString();
 const myNode = getNodeString();
 const myTag = getTagString();
 const myNav = getNavString();
+const myFooter = getFooterString();
 
 //const myHero = getHeroString();
 const myNavigationBar = getNavigationBarString();
@@ -19,4 +19,5 @@ const myNavigationBar = getNavigationBarString();
 export const NodePage = () => myNav + myNode;
 export const TagPage = () => myNav + '<div class="column-2">' + myTag + '</div>';
 export const TagPageWithHeader = () => myNavigationBar + '<div id="apl-content">' + myNav + '<div class="column-2">' + myTag + '</div></div>';
+export const NodePageWithHeaderAndFooter = () => myNavigationBar + '<div id="apl-content">' + myNav + '<div class="column-2">' + myNode + '</div>' + myFooter + '</div>';
 export const CalendarPage = () => myCalendar;
