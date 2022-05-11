cd131bdb12011f00827c1826d07310ece11b701a
Bryce Benton <you@example.com>
2022-05-11 12:14:38 -0500

    events calendar

--- src/stories/EventPage.stories.js
+++ src/stories/EventPage.stories.js
@@ -1,5 +1,5 @@
 //import { createEventPage } from './EventPage';
-import { getNodeString, getNavString, getTagString } from './Event';
+import { getNodeString, getNavString, getTagString, getCalendarString } from './Event';
 
 export default {
   title: 'Events/EventPage',
@@ -7,9 +7,11 @@ export default {
 
 //const Template = (args) => createEventPage(args);
 
+const myCalendar = getCalendarString();
 const myNode = getNodeString();
 const myTag = getTagString();
 const myNav = getNavString();
 
 export const NodePage = () => myNav + myNode;
 export const TagPage = () => myNav + '<div class="column-2">' + myTag + '</div>';
+export const CalendarPage = () => myCalendar;
