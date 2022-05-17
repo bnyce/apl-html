d7425972cd4544229dc18d9fef362695becaa8b1
Bryce Benton <you@example.com>
2022-05-17 15:26:32 -0500

    Events homepage sidebar

--- src/stories/Sidebar.stories.js
+++ src/stories/Sidebar.stories.js
@@ -1,4 +1,4 @@
-import { createSidebar, getSidebarString, getHomePageButtons } from './Sidebar';
+import { createSidebar, getSidebarString, getHomePageButtons, getHomePageEvents } from './Sidebar';
 //import './exhibits-sidebar.css';
 import { createEvent, getNodeString, getNavString } from './Event';
 import { getExhibitNavString } from './Exhibit';
@@ -10,6 +10,7 @@ export default {
 
 const mySidebar = getSidebarString();
 const myHomePageButtons = getHomePageButtons();
+const myHomePageEvents = getHomePageEvents();
 
 const myEventsNav = getNavString();
 const myExhbitNav = getExhibitNavString();
@@ -23,3 +24,4 @@ export const Blog = () => mySidebar;
 export const Events = () => myEventsNav;
 export const Exhibits = () => myExhbitNav;
 export const HomePageButtons = () => myHomePageButtons;
+export const HomePageEvents = () => myHomePageEvents;
